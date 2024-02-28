1028bef148e00fbf6c20350761bf70a3e1302792
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-28 13:03:53 +0100

    a

--- human.sh
+++ human.sh
@@ -1,13 +1,4 @@
   
 #!/bin/bash 
-for i in $(cat mcd_inputs.tfvars | grep public-ips):
-do
-  echo $i
-  myline=echo $i|awk -F"="{'print $2'}
-  echo $myline
-  #hostip=$(awk -F "=" {'print $2'} << $i)
-  #echo $hostip
-  #curl "$hostip:8080"
-  #ssh "ubuntu@$i" "ls /tmp; docker ps; pwd" 
-done
+
 
