14ce690fa22a1a7e1ae033fb431caac5f0fdd8c4
Jacopo Pianigiani <jpianigi@cisco.com>
2024-02-28 07:23:29 +0100

    Wed Feb 28 07:23:29 CET 2024

---
+++ main.tf
@@ -0,0 +1,106 @@
+locals{
+    corporate_subnets_list_vpc = cidrsubnets(var.vpc_corporate_office_cidr_block,[ for i in range(length(var.humans)): 8]...)
+}
+
+resource "aws_vpc" "teashop_office" {
+      cidr_block       = var.vpc_corporate_office_cidr_block
+         enable_dns_hostnames = true
+         enable_dns_support   = true
+
+         tags = {
+            Name = format("mcd-demo-%s-%s-%s","teashop_office","humans",var.tfrun_identifier)
+            Tier = "teashop-hq"
+            Application = var.application_name
+            ResourceGroup = var.tfrun_identifier
+
+         }
+      }
+
+resource "aws_subnet" "corporate_subnet" {  
+         count =length(var.environment_list)
+         vpc_id            = aws_vpc.teashop_office[count.index].id
+         cidr_block        = local.corporate_subnets_list_vpc[count.index]
+         availability_zone = var.az1
+
+         tags = {
+            Name = format("mcd-demo-%s-%s-%s-subnet-%s",var.environment_list[count.index],var.application_name,var.tfrun_identifier)
+            Tier = local.vpc_names[0]
+            Application = var.application_name
+            Environment = var.environment_list[count.index]
+            ResourceGroup =random_pet.tf_run.id
+
+         }
+      }
+
+resource "aws_security_group" "human_sg" {
+      name        = "human_sg"
+      description = "Allow anything to dev, val , prod"
+      vpc_id      = aws_vpc.teashop_office
+
+      tags = {
+         Name = format("mcd-demo-%s-humans-sg-%s",var.application_name,var.tfrun_identifier)
+         Tier = "humans"
+         Application = var.application_name
+         ResourceGroup = var.tfrun_identifier
+      }
+      }
+resource "aws_vpc_security_group_ingress_rule" "allow_in_ssh_ipv4_frontend" {
+      security_group_id = aws_security_group.frontend_sg.id
+      cidr_ipv4         = "0.0.0.0/0"
+      from_port         = 22
+      ip_protocol       = "tcp"
+      to_port           = 22
+      }
+
+      resource "aws_vpc_security_group_ingress_rule" "allow_in_1111_ipv4_frontend" {
+      security_group_id = aws_security_group.frontend_sg.id
+      cidr_ipv4         = "0.0.0.0/0"
+      from_port         = 1
+      ip_protocol       = "tcp"
+      to_port           = 65535
+      }
+      
+      resource "aws_vpc_security_group_ingress_rule" "allow_in_icmp_ipv4_frontend" {
+         security_group_id = aws_security_group.frontend_sg.id
+         cidr_ipv4         = aws_vpc.teashop_office.cidr_block
+         ip_protocol       = "icmp"
+         from_port         = -1
+         to_port           = -1
+      }
+
+      resource "aws_vpc_security_group_egress_rule" "allow_out_all_traffic_ipv4_frontend" {
+      security_group_id = aws_security_group.human_sg.id
+      cidr_ipv4         = "0.0.0.0/0"
+      ip_protocol       = "-1" # semantically equivalent to all ports
+      }
+
+
+      resource "aws_instance" "one_human" {
+        count =length(var.environment_list)
+        ami                     = var.ec2_instance_ami
+        instance_type           = var.ec2_instance_type_human
+        availability_zone       = var.az1
+         subnet_id               = aws_subnet.corporate_subnet[count.index]
+      
+         key_name                = var.keyname
+         associate_public_ip_address = true
+
+         vpc_security_group_ids  = [aws_security_group.human_sg]
+         root_block_device {
+            volume_size = 10 # in GB 
+            volume_type = "gp3"
+            encrypted   = false
+         } 
+         tags = {
+            Name = format("mcd-demo-humans-%s-%s-%s-%s",var.application_name,var.environment_list[count.index],var.humans[count.index],var.tfrun_identifier)
+            #Name = "mcd-demo-teashop-backend"
+            Tier = "backend"
+            Application = var.application_name
+            Environment = var.environment_list[count.index]
+            ResourceGroup = var.tfrun_identifier
+
+
+         }
+         #user_data = file("${path.module}/cloud-init-backend.yaml")
+       }
+       
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_corporate_office_cidr_block" { type = string}
+variable "tenant_cidr" {type = "string"}
+variable "humans" { type = list(string)}
+variable "application_name" { type = "string"}
+variable "tfrun_identifier" {type = "string"}
+variable "environment_list" {type = list(string)}
+variable "az1" {type = "string"}
+variable "ec2_instance_ami" {type = "string"}
+variable "ec2_instance_type_human" {type = "string"}
+variable "keyname" {type = "string"}
+
+# ec2 instance type
+variable "ec2_instance_type_human" {
+  description = "ec2 instance type"
+  type        = string
+  default     = "t2.small"
+}
+
+variable "humans" { default =  ["Jonny The Dev", "Sarah the Val","Brandon The Ops"] }
+
+
+
+
+
