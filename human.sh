4b0d9316b8cc0fd2e1608eadac5fdf64d7ac96ba
Jacopo Pianigiani <jpianigi@cisco.com>
2024-03-03 10:41:38 +0100

    a

--- human.sh
+++ human.sh
@@ -2,4 +2,4 @@
 #!/bin/bash 
  # 1>/dev/null 2>/dev/null
 frontend_fqdn=$1
-setsid locust -f /tmp/locustfile.py --headless -u 1 -r 1 --host http://$frontend_fqdn
+setsid /usr/bin/locust -f /tmp/locustfile.py --headless -u 1 -r 1 --host http://$frontend_fqdn
