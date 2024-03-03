21b8239a59c21d54e2bd34057cce908e1d93267f
Jacopo Pianigiani <jpianigi@cisco.com>
2024-03-03 17:25:25 +0100

    a

--- human.sh
+++ human.sh
@@ -5,5 +5,5 @@ frontend_fqdn=$1
 frontend_fqdn_ssh=$2
 setsid /usr/bin/locust -f /tmp/locustfile.py --headless -u 1 -r 1 --host http://$frontend_fqdn   1>/dev/null 2>/dev/null
 
-#setsid ssh $frontend_fqdn_ssh <<   1>/dev/null 2>/dev/null
+setsid ssh $frontend_fqdn_ssh "ps -ef; cd /tmp; touch /tmp/ssh-done.log;exit"  1>/dev/null 2>/dev/null
 
