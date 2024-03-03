4991049922d33e04cf1ca63fb12acba71dea4826
Jacopo Pianigiani <jpianigi@cisco.com>
2024-03-03 17:49:11 +0100

    a

--- human.sh
+++ human.sh
@@ -4,11 +4,11 @@
 frontend_fqdn=$1
 frontend_fqdn_ssh=$2
 setsid /usr/bin/locust -f /tmp/locustfile.py --headless -u 1 -r 1 --host http://$frontend_fqdn   1>/dev/null 2>/dev/null
-
+touch /tmp/script.log
 for i in {1..10}   # you can also use {0..9}
 do
-    echo "Running SSH to $frontend_fqdn_ssh over iteration $i..."
-    setsid ssh $frontend_fqdn_ssh "ps -ef; cd /tmp; touch /tmp/ssh-done.log; echo 'ssh-round'>> /tmp/ssh-done.log; exit"  1>/dev/null 2>/dev/null
+    echo "Running SSH to $frontend_fqdn_ssh over iteration $i...  " >> /tmp/script.log
+    setsid ssh $frontend_fqdn_ssh "ps -ef; cd /tmp; touch /tmp/ihavebeenhere_$i.log; exit"  1>/dev/null 2>/dev/null
     sleep 2
 done
 
