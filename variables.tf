ebcd6f904862716f3877d788f17b0733fba438fe
Jacopo Pianigiani <jpianigi@cisco.com>
2024-03-05 20:21:35 +0100

    a

--- variables.tf
+++ variables.tf
@@ -45,14 +45,14 @@ variable "humans" {
 }
 variable "frontend-nodes-public-fqdns" {
   type = list(string)
-  default=["ciscomcd-l-mcd-ctsmtgks-a69bcac4b46fec01.elb.us-east-1.amazonaws.com:8080",
-          "ciscomcd-l-mcd-ctsmtgks-a69bcac4b46fec01.elb.us-east-1.amazonaws.com:8180",
-          "ciscomcd-l-mcd-ctsmtgks-a69bcac4b46fec01.elb.us-east-1.amazonaws.com:8280"]
+  default=["ciscomcd-l-mcd-wrotceoz-64b8fd6b0cc70dcd.elb.us-east-1.amazonaws.com:8080",
+          "ciscomcd-l-mcd-wrotceoz-64b8fd6b0cc70dcd.elb.us-east-1.amazonaws.com:8180",
+          "ciscomcd-l-mcd-wrotceoz-64b8fd6b0cc70dcd.elb.us-east-1.amazonaws.com:8280"]
 }
 
 variable "frontend-nodes-public-fqdns_ssh" {
   type = list(string)
-  default=["ciscomcd-l-mcd-ctsmtgks-a69bcac4b46fec01.elb.us-east-1.amazonaws.com:8022",
-          "ciscomcd-l-mcd-ctsmtgks-a69bcac4b46fec01.elb.us-east-1.amazonaws.com:8122",
-          "ciscomcd-l-mcd-ctsmtgks-a69bcac4b46fec01.elb.us-east-1.amazonaws.com:8222"]
+  default=["ciscomcd-l-mcd-wrotceoz-64b8fd6b0cc70dcd.elb.us-east-1.amazonaws.com:8022",
+          "ciscomcd-l-mcd-wrotceoz-64b8fd6b0cc70dcd.elb.us-east-1.amazonaws.com:8122",
+          "ciscomcd-l-mcd-wrotceoz-64b8fd6b0cc70dcd.elb.us-east-1.amazonaws.com:8222"]
 }
