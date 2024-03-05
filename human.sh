0a148ca998e1ecb53d11ad687daac7bcdef6bb10
Jacopo Pianigiani <jpianigi@cisco.com>
2024-03-05 20:32:06 +0100

    s

--- human.sh
+++ human.sh
@@ -3,7 +3,7 @@
  # 1>/dev/null 2>/dev/null
 frontend_fqdn=$1
 frontend_fqdn_ssh=$2
-setsid /usr/bin/locust -f /tmp/locustfile.py --headless -u 10 -r 5 --host http://$frontend_fqdn   1>/dev/null 2>/dev/null
+setsid /usr/bin/locust -f /tmp/locustfile.py --headless -u 15 -r 1 --host http://$frontend_fqdn   1>/dev/null 2>/dev/null
 
 touch /tmp/script.log
 
