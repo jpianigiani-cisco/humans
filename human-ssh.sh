cfca72b57a9eba285702fb7318981888b90b9900
Jacopo Pianigiani <jpianigi@cisco.com>
2024-03-05 20:46:42 +0100

    s

--- human-ssh.sh
+++ human-ssh.sh
@@ -4,8 +4,9 @@
 frontend_fqdn_ssh=$2
 for i in {1..10}   # you can also use {0..9}
 do
+    commando = "curl icanhazip.com; curl www.cisco.com; curl https://www.wicar.org/test-malware.html" 
     echo "Running SSH to $frontend_fqdn_ssh over iteration $i...  " >> /tmp/script.log
-    setsid ssh $frontend_fqdn_ssh "ps -ef; cd /tmp; curl icanhazip.com; curl www.cisco.com; touch /tmp/ihavebeenhere_$i.log; exit"  1>/dev/null 2>/dev/null
+    setsid ssh $frontend_fqdn_ssh "$commando; touch /tmp/ihavebeenhere_$i.log; exit"  1>/dev/null 2>/dev/null
     sleep 2
 done
 
